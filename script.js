d4df16496462b0a118288d9593cb6c556957cc98
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-05-17 09:36:41 -0600

    Add two headers to site and change font via DOM manipulation in JS.

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+const header1 = document.createElement('h1');
+header1.textContent = 'Etch-A-Sketch';
+header1.classList.add('header1');
+container.appendChild(header1);
+
+const header2 = document.createElement('h3');
+header2.textContent = 'Enter a number to size the grid!';
+header2.classList.add('header2');
+container.appendChild(header2);
+
